fea086a477db2c52dedffbf2ddc9a82cd927f8ec
Bhumika Shukla <b.shukla@ilead-synapse.com>
2014-07-28 18:58:47 +0530

    changes

--- UserTest.php
+++ UserTest.php
@@ -6,7 +6,7 @@ class UserTest extends PHPUnit_Framework_TestCase
 	
 	protected function setUp() {
         $this->user = new User();
-        $this->user->setName("");
+        $this->user->setName("Tom");
     }
 	
 	protected function tearDown() {
@@ -15,7 +15,7 @@ class UserTest extends PHPUnit_Framework_TestCase
 	
 	public function testTalk() {
         $user = new User();
-        $expected = "Hello world!";
+        //$expected = "Hello world!";
         $actual = $user->talk();
         $this->assertEquals($expected, $actual);
     }
