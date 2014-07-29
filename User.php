33d0182b10b9765b11ff249cb512b59422d166dd
Bhumika Shukla <b.shukla@ilead-synapse.com>
2014-07-29 13:08:15 +0530

    changes

--- User.php
+++ User.php
@@ -5,7 +5,14 @@ class User {
         return $this->name;
     }
     public function setName($name) {
-        $this->name = $name;
+		if($name == "")
+		{
+			echo 'error bhumika';
+		}
+		else
+		{
+			$this->name = $name;
+		}
     }
     public function talk() {
         return "Hello world!";
--- UserTest.php
+++ UserTest.php
@@ -17,7 +17,7 @@ class UserTest extends PHPUnit_Framework_TestCase
 	public function testTalk() {
         $user = new User();
         $expected = "Hello world!";
-        //$actual = $user->talk();
+        $actual = $user->talk();
         $this->assertEquals($expected, $actual);
     }
 }
