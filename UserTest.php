8667a1c20349020d53c9a85ce5728777b5e5bc76
Bhumika Shukla <b.shukla@ilead-synapse.com>
2014-07-29 12:56:56 +0530

    changes

--- UserTest.php
+++ UserTest.php
@@ -16,7 +16,7 @@ class UserTest extends PHPUnit_Framework_TestCase
 	public function testTalk() {
         $user = new User();
         $expected = "Hello world!";
-        $actual = $user->talk();
+        //$actual = $user->talk();
         $this->assertEquals($expected, $actual);
     }
 }
